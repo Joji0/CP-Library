f12c1afecaf592070bb678e5a18da880233666dc
Joji0 <jonathankw2005@gmail.com>
2026-01-19 15:09:18 +0700

    Add A+B sample with docs

---
+++ library/sample/A+B.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <cstdint>
+
+/**
+ * @brief returns the sum of two integers
+ * @param a is the first integer and b is the second one
+ * @return sum of a and b
+ */
+int64_t sum(int64_t a, int64_t b) { return a + b; }
---
+++ test/sample/A+B.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include "../../library/sample/A+B.hpp"
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+        int64_t a, b;
+        cin >> a >> b;
+        cout << sum(a, b) << '\n';
+}
+
+int main() {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int t = 1;
+        // cin >> t;
+        while (t--) solve();
+
+        return 0;
+}
